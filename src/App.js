b3e7efd09201880092cd37d9fcac1e251d5b8797
Caio Toledo <ctoledo@pieracciani.com.br>
2023-09-14 00:06:16 -0300

    criando botões com icones do mui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <KeyboardArrowUpIcon sx={{ fontSize: 200 }} />
+      </div>
+      <div>
+        <KeyboardArrowLeftIcon
+          sx={{ fontSize: 200 }}
+          style={{ padding: "5px 60px" }}
+        />
+        <KeyboardArrowRightIcon
+          sx={{ fontSize: 200 }}
+          style={{ padding: "5px 60px" }}
+        />
+      </div>
+      <div>
+        <KeyboardArrowDownIcon sx={{ fontSize: 200 }} />
+      </div>
     </div>
   );
 }
